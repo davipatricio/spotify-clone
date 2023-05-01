935d0d2b28d0467c7e5c6f3293339630b336262f
Davi Patricio <davipatricio2006@gmail.com>
2023-05-01 01:00:49 +0000

    add react-router-dom

--- src/layout.tsx
+++ src/layout.tsx
@@ -7,7 +7,13 @@ const Root = styled.div`
   display: grid;
   grid-template-columns: auto 1fr;
 
+  main > #content {
+    padding: 0 2rem;
+  }
+
+  /* Music bar */
   > footer {
+    /* Make music bar use the entire grid */
     grid-column: 1 / 3;
   }
 `;
@@ -17,15 +23,16 @@ const Main = styled.main`
   height: calc(100vh - 90px);
 `;
 
-export default function App() {
+export default function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <Root>
       <LateralNavbar />
 
       <Main>
         <Header />
-        <h1>Hello, world!</h1>
+        <div id="content">{children}</div>
       </Main>
+
       <MusicBar />
     </Root>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import GlobalStyles from './styles/GlobalStyles.ts';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import 'react-tooltip/dist/react-tooltip.css';
+import Layout from './layout.tsx';
+import Index from './routes/index.tsx';
 
-ReactDOM.createRoot(document.querySelector('body') as HTMLElement).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Index />,
+  },
+]);
+
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+ReactDOM.createRoot(document.querySelector('body')!).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyles />
-      <App />
+      <Layout>
+        <GlobalStyles />
+        <RouterProvider router={router} />
+      </Layout>
     </ThemeProvider>
   </React.StrictMode>
 );
